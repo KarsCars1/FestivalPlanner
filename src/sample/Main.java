f6499e14bfc034e9843ac04bb03b5af5e1b73082
Oliver Norton <o.k.norton21@gmail.com>
2022-02-10 11:01:55 +0100

    Added all Vbox components and a working list

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,9 @@ import javafx.geometry.Orientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -15,23 +17,29 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 500));
-        primaryStage.show();
-
         BorderPane agendaBorderpane = new BorderPane();
+
+        TextField textField = new TextField();
+
         VBox performerVBox = new VBox();
+        Label performerLabel = new Label("Performers:");
         Button addPerformer = new Button("Add performer");
         Button removePerformer = new Button("Remove performer");
         Button updatePerfomer = new Button("Update performer");
+
         ListView<String> performerlist = new ListView();
-        performerlist.setOrientation(Orientation.HORIZONTAL);
+        performerlist.getItems().addAll("test", "test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test");
+        performerlist.setOrientation(Orientation.VERTICAL);
 
-        performerVBox.getChildren().addAll(performerlist, addPerformer,updatePerfomer,removePerformer);
+        performerVBox.getChildren().addAll(performerLabel, performerlist, addPerformer, updatePerfomer, removePerformer);
         agendaBorderpane.setRight(performerVBox);
-
+        agendaBorderpane.setLeft(textField);
+        Scene agendaScene = new Scene(agendaBorderpane);
+        primaryStage.setScene(agendaScene);
+        primaryStage.show();
     }
 
 
