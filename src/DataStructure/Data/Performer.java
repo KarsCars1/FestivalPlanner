b7e887e623c5464bd17c45409aa4f23dc6686276
Stijn Custers <stijn657@gmail.com>
2022-04-01 12:03:47 +0200

    added Popularity
    added a popularity double to the performer to 
    be able to simulate  the flow during the festival

--- src/DataStructure/Data/Performer.java
+++ src/DataStructure/Data/Performer.java
@@ -4,8 +4,10 @@ import java.io.Serializable;
 
 public class Performer implements Serializable {
     protected String performerName;
-
-    public Performer(String performerName) {
+    private double popularity;
+    
+    public Performer(String performerName, double popularity) {
+        this.popularity = popularity/100.0;
         this.performerName = performerName;
     }
 
@@ -13,7 +15,15 @@ public class Performer implements Serializable {
         return performerName;
     }
 
+    public double getPopularity(){
+    return popularity * 100;
+    }
+
     public void setPerformerName(String performerName) {
         this.performerName = performerName;
+    }
+
+    public void setPopularity(double popularity){
+        this.popularity = popularity/100.0;
     }
 }
